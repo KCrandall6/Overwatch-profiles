f83362ad8c393a41116a65e33b3e0d50cdff2dcd
KCrandall6 <kcrandall67@gmail.com>
2023-01-14 21:12:44 -0700

    fixed bug where modal would not close after being opened; added bio info to players modal

--- src/pages/Characters/Hero.jsx
+++ src/pages/Characters/Hero.jsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import {Button, Modal} from 'react-bootstrap';
+import React, {useState} from 'react';
+import {Card, Button, Modal} from 'react-bootstrap';
 
-const Hero = ({hero, show, handleClose}) => {
+const Hero = ({hero}) => {
 
-  // const handleClose = () => setShow(false);
-  // const handleShow = () => setShow(true);
-  
-  return (
-    <>
-      <Modal show={show} onHide={handleClose} onClick={() => handleClose()}>
-        <Modal.Header closeButton>
+  const [show, setShow] = useState(false);
+  const [heroData, setHeroData] = useState({});
+  let summary = '';
+
+  const handleShow = () => {
+    setShow(!show);
+    fetch(`https://overfast-api.tekrop.fr/heroes/${hero.key}`)
+      .then((res) => res.json())
+      .then((res) => setHeroData(res))
+  };
+
+  const separate = () => {
+    if (heroData.location) {
+      summary = heroData.story.summary
+    }
+  }
+
+  separate();
+
+  return(
+    <div onClick={handleShow} >
+      <Card className="char-card mt-3 ms-2 me-2" variant="light">
+      <Modal show={show} onHide={handleShow}>
+        <Modal.Header closeButton onClick={handleShow}>
           <Modal.Title>{hero.name}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>{hero.role}</Modal.Body>
+        <Modal.Body className="d-flex flex-row">
+          <img
+            alt='Overwatch Hero'
+            src={hero.portrait}
+            height='100'
+            />
+          <div className="d-flex flex-column ps-5 pt-3">
+            <h1 className='card-title'>{hero.name}</h1>
+            <p className="mb-2 text-muted fst-italic">{hero.role}</p>
+            <p><b>Location: </b>{heroData.location}</p>
+          </div>
+        </Modal.Body>
+        <Modal.Body>
+          <p><b>Story: </b>{summary}</p>
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={() => handleClose()}>
+          <Button variant="secondary" onClick={handleShow}>
             Close
           </Button>
         </Modal.Footer>
       </Modal>
-    </>
-  );
-}
+
+        <Card.Body className="d-flex flex-row">
+          <img
+            alt='Overwatch Hero'
+            src={hero.portrait}
+            height='100'
+            />
+          <div className="d-flex flex-column ps-5 pt-3">
+            <Card.Title className='card-title'>{hero.name}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted fst-italic">{hero.role}</Card.Subtitle>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  )
+};
 
 export default Hero;
