2d9e623cd8ab2f2e93c0f64f8ddb073553aacd87
KCrandall6 <kcrandall67@gmail.com>
2023-01-15 02:49:24 -0700

    added buttons to sort and filter the results in characters

--- src/pages/Characters/Characters.jsx
+++ src/pages/Characters/Characters.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import {Dropdown} from 'react-bootstrap';
 import CharacterCard from './Hero';
 
 const Characters = () => {
@@ -6,17 +7,64 @@ const Characters = () => {
   const [charList, setCharList] = useState([]);
   const [searched, setSearched] = useState(false);
 
-  useEffect(() => {
+  const [originalCharList, setOriginalCharList] = useState([]);
+
+useEffect(() => {
     if (searched === false) {
-      setSearched(true);
-      fetch('https://overfast-api.tekrop.fr/heroes')
-      .then((res) => res.json())
-      .then((res) => setCharList(res))
+        setSearched(true);
+        fetch('https://overfast-api.tekrop.fr/heroes')
+        .then((res) => res.json())
+        .then((res) => {
+            setOriginalCharList(res);
+            setCharList(res);
+        });
     }
-  }, [searched]);
+}, [searched]);
+
+const sortBy = (type) => {
+  originalCharList.sort((a, b) => {
+      if (a[type] < b[type]) {
+          return -1;
+      }
+      if (a[type] > b[type]) {
+          return 1;
+      }
+      return 0;
+  });
+  setOriginalCharList([...originalCharList]);
+  setCharList(originalCharList);
+}
+
+const filterBy = (type) => {
+  const filteredCharList = originalCharList.filter((hero) => hero.role === type);
+  setCharList(filteredCharList);
+}
   
   return (
     <div>
+      <div className="d-flex flex-row justify-content-center m-2">
+        <Dropdown className="me-3">
+          <Dropdown.Toggle style={{ color:"black", backgroundColor: "#F99B12", border: "none"}} id="dropdown-basic">
+            Sort by
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={() => sortBy('name')}>Name</Dropdown.Item>
+            <Dropdown.Item onClick={() => sortBy('role')}>Role</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Dropdown >
+          <Dropdown.Toggle style={{ color:"black", backgroundColor: "#F99B12", border: "none"}} id="dropdown-basic">
+            Filter by
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={() => filterBy('support')}>Support</Dropdown.Item>
+            <Dropdown.Item onClick={() => filterBy('damage')}>Damage</Dropdown.Item>
+            <Dropdown.Item onClick={() => filterBy('tank')}>Tank</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+
       {charList.map((hero) => {
         return (
           <div key={hero.name} >
--- src/pages/Characters/Hero.jsx
+++ src/pages/Characters/Hero.jsx
@@ -6,6 +6,11 @@ const Hero = ({hero}) => {
   const [show, setShow] = useState(false);
   const [heroData, setHeroData] = useState({});
   let summary = '';
+  let additionalPics = {
+    "first": '',
+    "second": '',
+    "third": ''
+  };
 
   const handleShow = () => {
     setShow(!show);
@@ -17,14 +22,16 @@ const Hero = ({hero}) => {
   const separate = () => {
     if (heroData.location) {
       summary = heroData.story.summary
+      additionalPics.first = heroData.story.chapters[0].picture
+      additionalPics.second = heroData.story.chapters[1].picture
     }
   }
-
   separate();
+
 
   return(
     <div onClick={handleShow} >
-      <Card className="char-card mt-3 ms-2 me-2" variant="light">
+      <Card className="char-card mb-3 ms-2 me-2" variant="light">
       <Modal show={show} onHide={handleShow}>
         <Modal.Header closeButton onClick={handleShow}>
           <Modal.Title>{hero.name}</Modal.Title>
@@ -33,16 +40,30 @@ const Hero = ({hero}) => {
           <img
             alt='Overwatch Hero'
             src={hero.portrait}
-            height='100'
+            height='200'
             />
-          <div className="d-flex flex-column ps-5 pt-3">
-            <h1 className='card-title'>{hero.name}</h1>
-            <p className="mb-2 text-muted fst-italic">{hero.role}</p>
-            <p><b>Location: </b>{heroData.location}</p>
+          <div className="d-flex flex-column overflow-auto text-center">
+            <p className='modal-title'><b>{hero.name}</b></p>
+            <p className="mb-2 text-muted fst-italic text-center">{hero.role}</p>
+            <p className="location text-start"><b>Location: </b>{heroData.location}</p>
           </div>
         </Modal.Body>
         <Modal.Body>
           <p><b>Story: </b>{summary}</p>
+        </Modal.Body>
+        <Modal.Body>
+          <img
+            alt='Overwatch Hero'
+            src={additionalPics.first}
+            width='100%'
+            />
+        </Modal.Body>
+        <Modal.Body>
+          <img
+            alt='Overwatch Hero'
+            src={additionalPics.second}
+            width='100%'
+            />
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleShow}>
